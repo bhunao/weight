4ffaa53e7c2a9f26c502da1c08c75d72bc88a2b0
felix <bruno.felix@e-deploy.com.br>
2022-01-14 15:37:50 -0300

    new pages

--- back-end/crud.py
+++ back-end/crud.py
@@ -16,5 +16,5 @@ def create_user_weight(db: Session, weight: schemas.WeightCreate):
     db.refresh(db_weight)
     return db_weight
 
-def get_weight_by_id(db: Session, id: int):
-    return db.query(models.Weight).filter(models.Weight.id == id).first()
+def get_weight_by_name(db: Session, name: str):
+    return db.query(models.Weight).filter(models.Weight.name == name).all()
--- back-end/main.py
+++ back-end/main.py
@@ -24,14 +24,32 @@ def get_db():
 
 
 @app.get("/", response_class=HTMLResponse)
-def load_weights(request: Request, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def home(request: Request, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     weight_list = crud.get_weights(db, skip=skip, limit=limit)
     return templates.TemplateResponse("home.html", {"request": request, "weight_list": weight_list})
 
 
 @app.post("/", response_class=HTMLResponse)
-def weights(request: Request, name: str = Form(...), weight: str = Form(...), skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def home(request: Request, name: str = Form(...), weight: str = Form(...), skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     person = schemas.WeightCreate(name=name, weight=float(weight))
-    w = crud.create_user_weight(db, person)
+    crud.create_user_weight(db, person)
     weight_list = crud.get_weights(db, skip=skip, limit=limit)
     return templates.TemplateResponse("home.html", {"request": request, "weight_list": weight_list})
+
+
+@app.post("/", response_class=HTMLResponse)
+def search(request: Request, name_search: str = Form(...), db: Session = Depends(get_db)):
+    weight_list = crud.get_weight_by_name(db, name_search)
+    return templates.TemplateResponse("weight.html", {"request": request, "weight_list": weight_list, "name": name_search})
+
+
+@app.get('/w', response_class=HTMLResponse)
+def weight(request: Request, name: str, db: Session = Depends(get_db)):
+    weight_list = crud.get_weight_by_name(db, name)
+    return templates.TemplateResponse("weight.html", {"request": request, "weight_list": weight_list, "name": name})
+
+
+@app.get('/test')
+def test(name_search: str):
+    print(f'test worked {name_search=}')
+    return name_search
