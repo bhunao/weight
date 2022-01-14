62dd2246e8c9e99e02a502fcee5d0c5a656e5df0
felix <bruno.felix@e-deploy.com.br>
2022-01-14 12:54:30 -0300

    changes

--- back-end/main.py
+++ back-end/main.py
@@ -23,13 +23,13 @@ def get_db():
         db.close()
 
 
-@app.get("/home/", response_class=HTMLResponse)
+@app.get("/", response_class=HTMLResponse)
 def load_weights(request: Request, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     weight_list = crud.get_weights(db, skip=skip, limit=limit)
     return templates.TemplateResponse("home.html", {"request": request, "weight_list": weight_list})
 
 
-@app.post("/home/", response_class=HTMLResponse)
+@app.post("/", response_class=HTMLResponse)
 def weights(request: Request, name: str = Form(...), weight: str = Form(...), skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     person = schemas.WeightCreate(name=name, weight=float(weight))
     w = crud.create_user_weight(db, person)
